bf7cfbce042666221f1d9eb4bcd24bb9b6575a11
Apeksha Rathod <apeksha.rathod@globalvoxinc.com>
2025-11-03 17:31:31 +0530

    Initial commit with client and server folders

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const e = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const connectDB = require('./config/db');
+connectDB();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
